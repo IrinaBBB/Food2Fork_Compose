5975ebfac1df750c74adc48298ceb6c35b1e858e
Irina <balkarovairina@gmail.com>
2021-08-02 10:42:26 +0200

    test: changing button colors

--- app/src/main/java/ru/irinavb/food2fork/MainActivity.kt
+++ app/src/main/java/ru/irinavb/food2fork/MainActivity.kt
@@ -3,36 +3,41 @@ package ru.irinavb.food2fork
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.irinavb.food2fork.ui.theme.Food2ForkTheme
+import ru.irinavb.food2fork.ui.theme.Purple200
+import ru.irinavb.food2fork.ui.theme.Purple700
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Food2ForkTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Hey look some text")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Yellow
+                    )
+                ) {
+                    Text(text = "A Button")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Food2ForkTheme {
-        Greeting("Android")
-    }
-}
