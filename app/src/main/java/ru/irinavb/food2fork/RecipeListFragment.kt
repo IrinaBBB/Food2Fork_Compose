1b8fea915c3eac998815b5a43e2ce41bcb99ff64
Irina <balkarovairina@gmail.com>
2021-08-04 13:03:33 +0200

    add: domain data model for a recipe

--- app/src/main/java/ru/irinavb/food2fork/RecipeListFragment.kt
+++ app/src/main/java/ru/irinavb/food2fork/RecipeListFragment.kt
@@ -1,32 +1,21 @@
 package ru.irinavb.food2fork
 
-import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 
 class RecipeListFragment : Fragment() {
 
@@ -47,11 +36,6 @@ class RecipeListFragment : Fragment() {
                     Spacer(modifier = Modifier.padding(10.dp))
                     Button(
                         onClick = {
-//                            val navHostFragment = activity?.supportFragmentManager!!
-//                                .findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
-//                            val navController = navHostFragment.findNavController()
-//                            navController.navigate(R.id.viewRecipe)
-
                            findNavController().navigate(R.id.viewRecipe)
                         }
                     ) {
@@ -62,3 +46,4 @@ class RecipeListFragment : Fragment() {
         }
     }
 }
+
